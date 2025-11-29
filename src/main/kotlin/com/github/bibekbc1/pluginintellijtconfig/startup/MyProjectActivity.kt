4ff0857adcd236dfe32eb2f7187601f93a95acbe
GitHub Action <action@github.com>
2025-11-29 07:41:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/bibekbc1/pluginintellijtconfig/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/bibekbc1/pluginintellijtconfig/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.bibekbc1.pluginintellijtconfig.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/bibekbc1/pluginintellijtconfig/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bibekbc1/pluginintellijtconfig/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bibekbc1.pluginintellijtconfig.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bibekbc1.pluginintellijtconfig.MyBundle
+import com.github.bibekbc1.pluginintellijtconfig.services.MyProjectService
 import javax.swing.JButton
 
 
